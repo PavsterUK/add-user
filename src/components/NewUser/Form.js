dee52745c1f422f1f6320481d93a8b731ea99d9e
PavsterUK <pavelnaumovic@gmail.com>
2022-01-13 22:03:22 +0000

    validation

--- src/components/NewUser/Form.js
+++ src/components/NewUser/Form.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
 import styles from "./Form.module.css";
+import OverlayWindow from "../UI/OverlayWindow"
 
 const Form = (props) => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [errors, setErrors] = useState("");
+
 
   const nameChangeHandler = (event) => {
     setName(event.target.value);
@@ -16,6 +20,13 @@ const Form = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    let error = validate();
+    if (error !== "") {
+      setIsInvalid(true);
+      setErrors(error);
+      return
+    }
+
     const user = {
       name: name,
       age: +age,
@@ -26,27 +37,43 @@ const Form = (props) => {
     setAge("");
   };
 
+  const validate = () => {
+    if (age < 0) {
+      return "Age should be more than 0"
+    } else if (age === "" || name === "") {
+      return "Age or Name must not be blank"
+    } else {
+      return "" //No error
+    }
+    
+  }
+  
   return (
-    <form onSubmit={submitHandler} className={styles.form}>
-      <label>Username</label>
-      <br />
-      <input
-        type="text"
-        placeholder="eg. John"
-        onChange={nameChangeHandler}
-        value={name}
-      />
-      <br />
-      <label>Age(Years)</label>
-      <br />
-      <input
-        type="number"
-        placeholder="eg. 22"
-        onChange={ageChangeHandler}
-        value={age}
-      />
-      <Button type="submit" title="Add User" />
-    </form>
+    <div>
+      <form onSubmit={submitHandler} className={styles.form}>
+        <label>Username</label>
+        <br />
+        <input
+          type="text"
+          placeholder="eg. John"
+          onChange={nameChangeHandler}
+          value={name}
+        />
+        <br />
+        <label>Age(Years)</label>
+        <br />
+        <input
+          type="number"
+          placeholder="eg. 22"
+          onChange={ageChangeHandler}
+          value={age}
+        />
+        <Button type="submit" title="Add User" />
+      </form>
+
+      {isInvalid && <OverlayWindow paragraph={errors} setIsInvalid={setIsInvalid}/>}
+      
+    </div>
   );
 };
 
